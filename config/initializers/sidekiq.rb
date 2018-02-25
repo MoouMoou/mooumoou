38e3d3545503bfca82b2357eac661a39a065d1ef
iuhoay <iuhoay@gmail.com>
2018-02-25 21:18:09 +0800

    sidekiq web auth

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,9 @@ schedule_file = "config/schedule.yml"
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
+
+require 'sidekiq/web'
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [Rails.application.secrets.sidekiq_username, Rails.application.secrets.sidekiq_password]
+end
